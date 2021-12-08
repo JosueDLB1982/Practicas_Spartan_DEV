783a96cc0b8a929bd91418781db5562ad1c6ddeb
Josue_Lopez <josuedlopez1982@gmail.com>
2021-12-07 23:35:39 -0300

    ejercicio de practica

--- Josue/dividirArray.js
+++ Josue/dividirArray.js
@@ -1,5 +1,9 @@
-const numbers = [10, 20, 30]
+// Crear una función que reciba como parámetro un array de valores numéricos y devuelva otro array númerico, cuyo contenido sean cada uno de los elementos del array dado divididos entre 2
 
-const dividirElementosEntreDos = () => {
-    
-}
+const numbers = [10, 20, 30]
+const numbers2 = [20, 50, 1000]
+const numbers3 = [14, 52, 70, 96]
+
+const newNumbers = number => result = number.map(number=>number/2)
+
+console.log(newNumbers(numbers3))
